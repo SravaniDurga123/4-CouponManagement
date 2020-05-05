612eb0e0aa690fd524ecda907fa33d048c74e42e
SravaniDurga123 <sravanidurga.maragani@gmail.com>
2020-05-05 20:13:28 +0530

    fifth

---
+++ TST_UserManagement/CouponManagementTestCase/Data/Coupon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponManagementTestCase.Data
+{
+    class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponNumber { get; set; }
+        public string CouponStatus { get; set; }
+        public DateTime CouponStartDate { get; set; }
+        public DateTime CouponExpiredDate { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int? UserId { get; set; }
+    }
+}
